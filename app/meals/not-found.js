84f3e92145a4ced4b0c15d9b95746c4c3e6497d4
Mazdak <mazdakxezri74@gmail.com>
2024-12-15 00:03:27 +0330

    first update

---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>unfortunately, We could not Find the requested page or Meal data.</p>
+    </main>
+  );
+};
+
+export default NotFound;
