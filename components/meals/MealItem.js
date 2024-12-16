42542f4d7f4756a9516c7a61fdfdfbd860c55d74
Mazdak <mazdakxezri74@gmail.com>
2024-12-16 15:33:05 +0330

    add delete

--- components/meals/MealItem.js
+++ components/meals/MealItem.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { deleteMeal } from "@/lib/meals";
 import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
@@ -19,6 +20,7 @@ export default function MealItem({ title, slug, image, summary, creator }) {
         <p className={classes.summary}>{summary}</p>
         <div className={classes.actions}>
           <Link href={`/meals/${slug}`}>View Details</Link>
+          <button onClick={() => deleteMeal(slug)}>Remove Meal</button>
         </div>
       </div>
     </article>
